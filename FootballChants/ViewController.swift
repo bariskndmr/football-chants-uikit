a5beda737c4a4117ec8206982ab90426f924a96d
bariskndmr <bariskandemirx@gmail.com>
2024-10-29 14:27:34 +0300

    first commit

---
+++ FootballChants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootballChants
+//
+//  Created by BarisKandemir on 29.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.view.backgroundColor = .systemRed
+  }
+
+
+}
+
